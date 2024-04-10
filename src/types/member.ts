43964383aae98f8535d9a6ac68a615ed7ba16029
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-04-11 02:57:29 +0900

    feat:型を定義

---
+++ src/types/member.ts
@@ -0,0 +1,10 @@
+import department from "./department"
+
+interface member {
+    name: string,
+    student_number: string,
+    department: department
+    mail: string,
+}
+
+export default member;
