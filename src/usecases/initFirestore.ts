d1b98aa3395c3d46c4ab87437eedb5d4b6db0cfc
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-04-11 03:06:41 +0900

    refactor: 初期化をtsに書き換え

--- src/usecases/initFirestore.ts
+++ src/usecases/initFirestore.ts
@@ -1,12 +1,12 @@
-function initializeFirestore() {
-    const admin = require('firebase-admin');
-    const serviceAccount = require('../../its-discord-auth-firebase-adminsdk-wn2uo-ac781d8325.json');
+import * as admin from 'firebase-admin';
+import serviceAccount from '../../its-discord-auth-firebase-adminsdk-wn2uo-ac781d8325.json';
 
+export function initializeFirestore(): admin.firestore.Firestore {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
     });
 
-    const db = admin.firestore();
+    const db: admin.firestore.Firestore = admin.firestore();
     return db;
 };
 
