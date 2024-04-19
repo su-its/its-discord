94a17fbd3cfdcee2c7951a78a10a7f1ad9b48d2f
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-04-19 16:25:57 +0900

    fix: 非同期処理を修正

--- src/usecases/insertMember.ts
+++ src/usecases/insertMember.ts
@@ -4,13 +4,14 @@ import { db } from "../infra/firebase";
 
 async function insertMember(member: Member) {
   const uniqueId = uuidv4();
-  const docRef = db.collection("members").doc(uniqueId);
+  const docRef = await db.collection("members").doc(uniqueId);
   await docRef.set({
     name: member.name,
     student_number: member.student_number,
     department: member.department,
     mail: member.mail,
   });
+  console.log("Document written with ID: ", docRef.id);
 }
 
 export default insertMember;
