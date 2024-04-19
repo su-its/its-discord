0421ea4169bd20494b70bcd212e8fee6ef984ef8
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-04-19 14:27:21 +0900

    feat: adminロールの初期化を実装

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -1,11 +1,22 @@
-import { Events } from "discord.js";
+import { Events, Guild } from "discord.js";
 import { CustomClient } from "../types/customClient";
+import initializeRoles from "../utils/initializeRoles";
 
 export function setupClientReadyHandler(client: CustomClient) {
   client.once(Events.ClientReady, () => {
     console.log("Ready!");
     if (client.user) {
       console.log(`Logged in as ${client.user.tag}`);
+
+
+      // ロールを初期化
+      const guild: Guild | undefined = client.guilds.cache.first();
+      if (!guild) {
+        console.error("No guild found.");
+        throw new Error("No guild found.");
+      } else {
+        initializeRoles(guild);
+      }
     }
   });
 }
