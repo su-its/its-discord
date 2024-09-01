ea1ddeada7202f96916157e4d6d77825c67dce9b
KinjiKawaguchi <kawakin0310@icloud.com>
2024-09-01 12:19:06 +0900

    refactor: 投稿チャンネルと時間を環境変数から設定

--- src/tasks/scheduleHotChannels.ts
+++ src/tasks/scheduleHotChannels.ts
@@ -3,12 +3,13 @@ import { CustomClient } from "../types/customClient";
 import { getHotChannels } from "../usecases/getHotChannels";
 import { TextChannel } from "discord.js";
 
-export function scheduleHotChannels(client: CustomClient) {
-  const job = new CronJob("0 0 * * *", async () => {
+export function scheduleHotChannels(client: CustomClient, channelId: string, time: string): void {
+  const job = new CronJob(time, async () => {
+    console.log("Job started");
     const guild = client.guilds.cache.first();
     if (guild) {
       const ranking = await getHotChannels(guild);
-      const channel = guild.channels.cache.find((ch) => ch.name === "hot-channels") as TextChannel;
+      const channel = guild.channels.cache.find((ch) => ch.id === channelId) as TextChannel;
 
       if (channel) {
         await channel.send(ranking);
